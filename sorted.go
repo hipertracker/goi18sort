c4d8572ce43fb33251100579a0192fb18085d10c
Jaroslaw Zabiello <hipertracker@gmail.com>
2020-02-27 21:45:52 +0000

    removed unnecessary return

--- sorted.go
+++ sorted.go
@@ -5,10 +5,9 @@ import (
 	"sort"
 )
 
-func sorted(arr []string, lang string) []string {
+func sorted(arr []string, lang string) {
 	less := collate.IndexString(lang)
 	sort.SliceStable(arr, func(i, j int) bool {
 		return less(arr[i], arr[j])
 	})
-	return arr
 }
