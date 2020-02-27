e9caed573d19900b9e0abc82e8e0b23e1bece467
Jaroslaw Zabiello <hipertracker@gmail.com>
2020-02-27 21:42:49 +0000

    shorted and 3x faster version

--- sorted.go
+++ sorted.go
@@ -5,26 +5,10 @@ import (
 	"sort"
 )
 
-type i18nStruct struct {
-	lang string
-	arr  []string
-}
-
-func (i18n i18nStruct) Less(i, j int) bool {
-	less := collate.IndexString(i18n.lang)
-	return less(i18n.arr[i], i18n.arr[j])
-}
-
-func (i18n i18nStruct) Len() int {
-	return len(i18n.arr)
-}
-
-func (i18n i18nStruct) Swap(i, j int) {
-	i18n.arr[i], i18n.arr[j] = i18n.arr[j], i18n.arr[i]
-}
-
 func sorted(arr []string, lang string) []string {
-	i18n := i18nStruct{lang, arr}
-	sort.Sort(i18n)
-	return i18n.arr
+	less := collate.IndexString(lang)
+	sort.SliceStable(arr, func(i, j int) bool {
+		return less(arr[i], arr[j])
+	})
+	return arr
 }
